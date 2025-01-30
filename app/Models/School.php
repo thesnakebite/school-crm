7abc8de2f0c8b718f0abbc567e29c432ff77d3e4
thesnakebite <alvaro@thesnakebite.es>
2025-01-30 10:52:13 +0100

    add schools migration and model structure

---
+++ app/Models/School.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class School extends Model
+{
+    /** @use HasFactory<\Database\Factories\SchoolFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'address',
+        'logo',
+        'email',
+        'phone',
+        'website'
+    ];
+}
