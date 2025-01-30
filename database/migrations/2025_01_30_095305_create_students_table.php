ff8e4d92d7719d75f813a68e5ed9f95b75579289
thesnakebite <alvaro@thesnakebite.es>
2025-01-30 10:56:11 +0100

    add students migration and model structure

---
+++ database/migrations/2025_01_30_095305_create_students_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('lastname');
+            $table->foreignId('school_id')->constrained()->onDelete('cascade');
+            $table->date('birthdate');
+            $table->string('hometown')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('students');
+    }
+};
